06db59e5edeb0a653e9ba33a2f90b430157ab913
tanvir <tanvir@tanvirs-MacBook-Air.local>
2025-01-08 21:46:19 +0600

    button and small js added

---
+++ index.js
@@ -0,0 +1,47 @@
+function search(){
+
+    let item = document.getElementById("meal").value ;
+
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${item}`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => search2(data));
+
+
+}
+
+function search2(data){
+
+    let allMeals = data.meals;
+
+    let disArea = document.getElementById("display");
+    disArea.textContent = " ";
+
+    if(allMeals.length > 5){
+        for(i = 1; i<= 5; i++){
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML = `Meal ID: ${allMeals[i-1].idMeal}`;
+
+            newDiv.classList.add("fix");
+            disArea.appendChild(newDiv);
+        }
+
+    }
+
+    else{
+
+        for(let i = 1; i <= allMeals.length;i++){
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML = `Meal ID: ${allMeals[i-1].idMeal}`;
+
+            newDiv.classList.add("fix");
+            disArea.appendChild(newDiv);
+        }
+
+    }
+
+
+
+
+}
